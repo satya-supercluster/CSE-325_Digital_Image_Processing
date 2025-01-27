35e6fa711f420160b1f0e70590a4a63edae51bc8
satya-supercluster <satya.supercluster@gmail.com>
2025-01-27 14:49:04 +0530

    LabAssignment-1

--- LabAssignment-1/Basic.m
+++ LabAssignment-1/Basic.m
@@ -1,34 +1,34 @@
-img = imread('image2.jpg');
-% imshow(img);
-% title('Original Image');
+img = imread('image.jpg');
+imshow(img);
+title('Original Image');
 
-% resized_img = imresize(img, 0.5);
-% imshow(resized_img);
-% title('Resized Image');
+resized_img = imresize(img, 0.5);
+imshow(resized_img);
+title('Resized Image');
 
 gray_img = rgb2gray(img);
-% imshow(gray_img);
-% title('Grayscale Image');
+imshow(gray_img);
+title('Grayscale Image');
 
-% bw_img = imbinarize(gray_img);
-% imshow(bw_img);
-% title('Black & White Image');
+bw_img = imbinarize(gray_img);
+imshow(bw_img);
+title('Black & White Image');
 
-% line_row = 100;
-% profile = gray_img(line_row, :);
-% figure;
-% plot(profile);
-% title('Image Profile');
-% xlabel('Column Index');
-% ylabel('Intensity Value');
+line_row = 100;
+profile = gray_img(line_row, :);
+figure;
+plot(profile);
+title('Image Profile');
+xlabel('Column Index');
+ylabel('Intensity Value');
 
-% R = img(:, :, 1);
-% G = img(:, :, 2);
-% B = img(:, :, 3);
-% figure;
-% subplot(1, 3, 1); imshow(R); title('Red Plane');
-% subplot(1, 3, 2); imshow(G); title('Green Plane');
-% subplot(1, 3, 3); imshow(B); title('Blue Plane');
+R = img(:, :, 1);
+G = img(:, :, 2);
+B = img(:, :, 3);
+figure;
+subplot(1, 3, 1); imshow(R); title('Red Plane');
+subplot(1, 3, 2); imshow(G); title('Green Plane');
+subplot(1, 3, 3); imshow(B); title('Blue Plane');
 
 reconstructed_img = cat(3, R, G, B);
 imshow(reconstructed_img);
